9f8f0c6b35ca4e16cd8eb414289f0fc97f7d303e
benzkittisak <benzkittisak@stv.ac.th>
2022-05-25 15:46:42 +0700

    update code and create new hooks

--- src/components/repositories-list/repositories-list.component.tsx
+++ src/components/repositories-list/repositories-list.component.tsx
@@ -1,20 +1,19 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
 
 import { useActions } from "../../hooks/useActions";
-import { RootState } from "../../redux";
+import { useTypedSelector } from "../../hooks/useTypedSelector";
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState("");
   const { searchRepositories } = useActions();
-  const {data , error , loading} = useSelector((state:RootState) => state.repositories);
+  const { data, error, loading } = useTypedSelector((state) => state.repositories);
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     // dispatch(actionCreators.searchRepositories(term));
     searchRepositories(term);
   };
-  
+
   return (
     <div>
       <form onSubmit={onSubmit}>
