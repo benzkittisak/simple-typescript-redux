43cff7c2008155a81910c4f5e2377644e095a736
benzkittisak <benzkittisak@stv.ac.th>
2022-05-25 15:36:59 +0700

    update code

--- src/components/repositories-list/repositories-list.component.tsx
+++ src/components/repositories-list/repositories-list.component.tsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+
+import { useActions } from "../../hooks/useActions";
 
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState("");
+  const { searchRepositories } = useActions();
+  const {data , error , loading} = useSelector((state:any) => state.repositories);
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // dispatch(actionCreators.searchRepositories(term));
+    searchRepositories(term);
+  };
+  
   return (
     <div>
-      <form>
-        <input type="text" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={term}
+          type="text"
+          onChange={(event) => setTerm(event.target.value)}
+        />
         <button>Search</button>
       </form>
     </div>
